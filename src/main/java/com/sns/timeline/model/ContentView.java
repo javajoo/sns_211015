1e4003be8daee60beca1f8799eb7980bffb88947
202-06\User <chu_veely@naver.com>
2022-03-02 17:46:50 +0900

    3/2 프로젝트 구현

--- src/main/java/com/sns/timeline/model/ContentView.java
+++ src/main/java/com/sns/timeline/model/ContentView.java
@@ -10,11 +10,11 @@ import com.sns.user.model.User;
 public class ContentView { // ${content.post.imagePath} 그 안의 객체의 필드를 사용할 수 있다
 	// 새로 만들어도 되지만 있는거 활용해도 된다.
 	private Post post;
-	private User user;
-
+	private User user; // 글쓴이
 	private List<CommentView> commentList;  // ${content.commentList.user.name}
-
-//	private List<Like> likeList;
+	private int likeCount; // 좋아요의 개수
+	private boolean filledLike; // 내가 좋아요를 눌렀는지의 여부
+	
 	
 	public Post getPost() {
 		return post;
@@ -38,6 +38,23 @@ public class ContentView { // ${content.post.imagePath} 그 안의 객체의 필
 
 	public void setCommentList(List<CommentView> commentList) {
 		this.commentList = commentList;
+	}
+
+	public int getLikeCount() {
+		return likeCount;
+	}
+
+	public void setLikeCount(int likeCount) {
+		this.likeCount = likeCount;
+	}
+
+	// boolean 인경우 is로 게터,세터 만들어진다.
+	public boolean isFilledLike() {
+		return filledLike;
+	}
+
+	public void setFilledLike(boolean filledLike) {
+		this.filledLike = filledLike;
 	}
 
 }
